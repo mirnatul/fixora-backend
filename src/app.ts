dfee7921a07520158c7428065a85fc3684f223a7
mirnatul <mdmirnatul@gmail.com>
2026-07-10 16:44:00 +0600

    env, and prisma setup with example user data get successfull

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,28 @@
+import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
 const app: Application = express();
 
+// middleware
+
+// for cookies credentials true (learn more)
+app.use(cors({ origin: config.app_url, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get("/", async (req: Request, res: Response) => {
-    res.send("Hello Fixora")
+    res.send("Hello, Prisma!");
 })
+
+// app.get("/user", async (req: Request, res: Response) => {
+//     const user = await prisma.user.findMany();
+//     console.log(user);
+//     res.send("Hello, Prisma!");
+// })
+
 
 export default app;
